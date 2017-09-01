bcc05f9d1efb0a949d8ae13e1cb4963ef9819afe
toshi_laptop_windows <toshiyasu.akazawa@gmail.com>
2017-09-01 14:28:39 -0700

    convert source

--- convert/convert.c
+++ convert/convert.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//#include <system.h>
 
 int main(int argc, char **argv){
-	char *command = "hello world";
+	char *command = "";
+	char *upcommand = "upcase|pwd";
+	char *lowcommand = "lowcase|pwd";
 
-	printf("%s",command);
+	if(argc>0){
+		int argNum=--argc;
+		do{
+			if(strcmp(argv[argNum], "-u") == 0){
+				command = upcommand;
+			}else if(strcmp(argv[argNum], "-l") == 0){
+				command = lowcommand;
+			}
+		}while(argNum-->0);
+	}
+	printf("command:%s\n", command);
 }
